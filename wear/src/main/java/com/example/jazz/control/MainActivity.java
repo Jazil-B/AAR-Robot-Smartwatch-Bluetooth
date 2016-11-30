7bd8e5127ec9c5754100924731df54af0ad1f889
Jazz <jazil.bekka@gmail.com>
2016-11-30 19:26:36 +0100

    update bluetooth

--- wear/src/main/java/com/example/jazz/control/MainActivity.java
+++ wear/src/main/java/com/example/jazz/control/MainActivity.java
@@ -4,10 +4,6 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -33,7 +29,6 @@ import java.util.Locale;
 import java.util.Set;
 import java.util.UUID;
 
-import static com.example.jazz.control.MainActivity.mBluetoothAdapter;
 
 public class MainActivity extends WearableActivity implements SensorEventListener {
 
@@ -49,7 +44,7 @@ public class MainActivity extends WearableActivity implements SensorEventListene
     private SensorManager sManager;
 
     private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
-    static BluetoothAdapter mBluetoothAdapter;
+    BluetoothAdapter mBluetoothAdapter;
     private Handler mHandler;
     private ServerConnectThread serverConnectThread;
     private ManageConnectThread manageConnectThread;
@@ -58,6 +53,8 @@ public class MainActivity extends WearableActivity implements SensorEventListene
     private BluetoothSocket socket = null;
     private InputStream receiveStream = null;// Canal de réception
     private OutputStream sendStream = null;// Canal d'émission
+    private ConnectThread connectThread;
+    static ConnectedThread connectedThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,14 +87,18 @@ public class MainActivity extends WearableActivity implements SensorEventListene
             if (pairedDevices.size() > 0) {
                 // Loop through paired devices
                 for (BluetoothDevice device : pairedDevices) {
-                    if(device.getName().contains("Jazz (Galaxy S4)")) {
+
+                    Log.d("Appaired with :","->+ "+device.getName());
+                    if(device.getName().contains("HC-05")) {
 
                         robot = device;
 
-                        serverConnectThread = new ServerConnectThread();
+                       // serverConnectThread = new ServerConnectThread(mBluetoothAdapter, UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
 
-                        serverConnectThread.acceptConnect(mBluetoothAdapter, UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+                        //serverConnectThread.run();
 
+                        connectThread = new ConnectThread(robot,UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"), mBluetoothAdapter);
+                        connectThread.run();
 
                     }
                 }
@@ -207,6 +208,9 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         super.onDestroy();
 
         mBluetoothAdapter.cancelDiscovery();
+
+        connectThread.cancel();
+        connectedThread.cancel();
     }
 
     public void onAccuracyChanged(Sensor arg0, int arg1)
@@ -231,48 +235,124 @@ public class MainActivity extends WearableActivity implements SensorEventListene
 }
 
 class ServerConnectThread extends Thread{
-    private BluetoothSocket bTSocket;
+    private BluetoothAdapter adapter;
+    UUID uuid;
+    BluetoothServerSocket serverSocket;
     ManageConnectThread manageConnectThread;
 
-    public ServerConnectThread() { }
+    public ServerConnectThread(BluetoothAdapter mBluetoothAdapter, UUID uuid) {
+        this.adapter = mBluetoothAdapter;
+        this.uuid = uuid;
+        BluetoothServerSocket tmp = null;
 
-    public void run() {
-        BluetoothServerSocket temp = null;
         manageConnectThread = new ManageConnectThread();
         try {
-            temp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("Service_Name", UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+            tmp  = adapter.listenUsingRfcommWithServiceRecord("Connect", uuid);
         } catch(IOException e) {
             Log.d("SERVERCONNECT", "Could not get a BluetoothServerSocket:" + e.toString());
         }
+
+        serverSocket = tmp;
+    }
+
+
+
+    public void run() {
+        BluetoothSocket bTSocket = null;
+
         while(true) {
+
             try {
-                bTSocket = temp.accept();
+                bTSocket = serverSocket.accept();
             } catch (IOException e) {
                 Log.d("SERVERCONNECT", "Could not accept an incoming connection.");
                 break;
             }
             if (bTSocket != null) {
+                Log.d("SmartWatch -> ","connect");
 
                 try {
                     manageConnectThread.sendData(bTSocket, 2);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+
+                try {
+                    serverSocket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                break;
+
 
             }
         }
     }
 
-    public void acceptConnect(BluetoothAdapter bTAdapter, UUID mUUID) {
-
-    }
 
     public void closeConnect() {
         try {
-            bTSocket.close();
+            serverSocket.close();
         } catch(IOException e) {
             Log.d("SERVERCONNECT", "Could not close connection:" + e.toString());
         }
+    }
+}
+
+class ConnectThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+    private BluetoothAdapter adapter;
+    UUID uuid;
+
+    public ConnectThread(BluetoothDevice device, UUID uuid, BluetoothAdapter adapter) {
+        this.adapter = adapter;
+        this.uuid = uuid;
+        // Use a temporary object that is later assigned to mmSocket,
+        // because mmSocket is final
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+
+        // Get a BluetoothSocket to connect with the given BluetoothDevice
+        try {
+            // MY_UUID is the app's UUID string, also used by the server code
+            tmp = device.createRfcommSocketToServiceRecord(uuid);
+        } catch (IOException e) { }
+        mmSocket = tmp;
+    }
+
+    public void run() {
+        // Cancel discovery because it will slow down the connection
+        adapter.cancelDiscovery();
+
+        try {
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and get out
+            try {
+                mmSocket.close();
+            } catch (IOException closeException) { }
+            return;
+        }
+
+        Log.d("Connexion", "Connected to "+mmDevice.getName());
+
+        MainActivity.connectedThread = new ConnectedThread(mmSocket);
+        MainActivity.connectedThread.write(1);
+        // Do work to manage the connection (in a separate thread)
+        //manageConnectedSocket(mmSocket);
+    }
+
+    /** Will cancel an in-progress connection, and close the socket */
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
     }
 }
 
@@ -293,5 +373,60 @@ class ManageConnectThread extends Thread {
         InputStream inputStream = socket.getInputStream();
         inputStream.read(buffer);
         return input.read();
+    }
+
+}
+
+class ConnectedThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final InputStream mmInStream;
+    private final OutputStream mmOutStream;
+
+    public ConnectedThread(BluetoothSocket socket) {
+        mmSocket = socket;
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+
+        // Get the input and output streams, using temp objects because
+        // member streams are final
+        try {
+            tmpIn = socket.getInputStream();
+            tmpOut = socket.getOutputStream();
+        } catch (IOException e) { }
+
+        mmInStream = tmpIn;
+        mmOutStream = tmpOut;
+    }
+
+    public void run() {
+        byte[] buffer = new byte[1024];  // buffer store for the stream
+        int bytes; // bytes returned from read()
+
+        // Keep listening to the InputStream until an exception occurs
+        while (true) {
+            try {
+                // Read from the InputStream
+                bytes = mmInStream.read(buffer);
+                // Send the obtained bytes to the UI activity
+                   /* mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();*/
+            } catch (IOException e) {
+                break;
+            }
+        }
+    }
+
+    /* Call this from the main activity to send data to the remote device */
+    public void write(int data) {
+        try {
+            mmOutStream.write(data);
+        } catch (IOException e) { }
+    }
+
+    /* Call this from the main activity to shutdown the connection */
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
     }
 }
